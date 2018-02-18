cd0ce9fb983eac06e3666083080a9c359ad4319c
Xian Chen <xianchen97@gmail.com>
2018-02-17 21:35:37 -0800

    Added test for functions, getting reverse equation values for example 4 - 2 = -2

--- RPNCalculator/RPNCalculator/Main.cpp
+++ RPNCalculator/RPNCalculator/Main.cpp
@@ -4,13 +4,39 @@
 
 int main()
 {
-	std::cout << "Enter your formula:\n";
-	std::string formula;
-	std::getline(std::cin, formula);
-	std::cout << "You entered " << formula << std::endl;
+
+	std::string stringAddition = "4 2 +";
+	std::string stringSubtraction = "4 2 -";
+	std::string stringMultiplication = "4 2 *";
+	std::string stringDivision = "4 2 /";
+	std::string complexEquation = " 2 - 3 * 4";
+	std::string complexEquation2 = "2 3 4 * -";
+	std::string complexEquation3 = "2 3 - 4 *";
+
+
+	//ADD
+	std::cout << "You entered " << stringAddition << " equal to 4 + 2" << std::endl;
 	RPNCalculator calculator;
-	int result = calculator.process_form(formula);
+	int result = calculator.process_form(stringAddition);
 	std::cout << "The result is:\n";
 	std::cout << result << std::endl;
+
+	//MULTIPLY
+	std::cout << "You entered " << stringMultiplication << " equal to 4 * 2" << std::endl;
+	result = calculator.process_form(stringMultiplication);
+	std::cout << "The result is:\n";
+	std::cout << result << std::endl;
+
+	//DIVIDE
+	std::cout << "You entered " << stringDivision << " equal to 4 / 2" << std::endl;
+	result = calculator.process_form(stringDivision);
+	std::cout << "The result is:\n";
+	std::cout << result << std::endl;
+
+	std::cout << "You entered " << stringSubtraction << " equal to 4 - 2" << std::endl;
+	result = calculator.process_form(stringSubtraction);
+	std::cout << "The result is:\n";
+	std::cout << result << std::endl;
+
 	system("pause");
 }
